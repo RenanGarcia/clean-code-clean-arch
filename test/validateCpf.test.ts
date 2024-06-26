6ba5babc92f6df3354ac3fddb1c0005b9b5640f1
Renan Garcia <renan.nobregarcia@gmail.com>
2024-06-25 21:28:29 -0300

    Covering all validateCpf scenarios before refactoring

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,21 @@
+import { validateCpf } from "~/validateCpf"
+
+test.each(["763.410.050-16", "71428793860", "87748248800"])(
+  "Deve testar se o cpf é valido %s",
+  (cpf: string) => {
+    const isValid = validateCpf(cpf)
+    expect(isValid).toBe(true)
+  },
+)
+
+test.each([
+  "",
+  null,
+  undefined,
+  "123456",
+  "1234567891011121314",
+  "11111111111",
+])("Deve testar se o cpf é inválido %s", (cpf: string) => {
+  const isValid = validateCpf(cpf)
+  expect(isValid).toBe(false)
+})
