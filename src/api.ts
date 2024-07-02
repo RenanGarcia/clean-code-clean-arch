9a5dca77b5518b8aed37173499512fcc5680e4c7
Renan Garcia <renan.nobregarcia@gmail.com>
2024-07-01 22:30:18 -0300

    Creating the api driver integration test

---
+++ src/api.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import { getAccount, signup } from "./signup"
+
+const app = express()
+app.use(express.json())
+
+app.post("/signup", async function (req, res) {
+  try {
+    const createdAccount = await signup(req.body)
+    return res.json(createdAccount)
+  } catch (err) {
+    return res.status(422).json({ message: err.message })
+  }
+})
+
+app.get("/accounts/:accountId", async function (req, res) {
+  try {
+    const account = await getAccount(req.params.accountId)
+    return res.json(account)
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
+})
+
+app.listen(3000)
---
+++ test/api.test.ts
@@ -0,0 +1,42 @@
+import axios from "axios"
+
+axios.defaults.validateStatus = function () {
+  return true
+}
+
+const BASE_URL = "http://localhost:3000"
+
+test("Deve criar uma conta para o passageiro", async function () {
+  const inputSignup = {
+    name: "João Pedro",
+    email: `test${Math.random()}@test.com.br`,
+    cpf: "264.500.550-06",
+    isPassenger: true,
+  }
+
+  const responseSignup = await axios.post(`${BASE_URL}/signup`, inputSignup)
+  const outputSignup = responseSignup.data
+  expect(outputSignup.accountId).toBeDefined()
+  const responseGetAccount = await axios.get(
+    `${BASE_URL}/accounts/${outputSignup.accountId}`,
+  )
+  const outputGetAccount = responseGetAccount.data
+  expect(outputGetAccount.accountId).toBe(outputSignup.accountId)
+  expect(outputGetAccount.name).toBe(inputSignup.name)
+  expect(outputGetAccount.email).toBe(inputSignup.email)
+  expect(outputGetAccount.cpf).toBe(inputSignup.cpf)
+  expect(outputGetAccount.isPassenger).toBe(inputSignup.isPassenger)
+})
+
+test("Não deve criar uma conta com cpf inválido", async function () {
+  const inputSignup = {
+    name: "João Pedro",
+    email: `test${Math.random()}@test.com.br`,
+    cpf: "26450111006123",
+    isPassenger: true,
+  }
+
+  const responseSignup = await axios.post(`${BASE_URL}/signup`, inputSignup)
+  const outputSignup = responseSignup.data
+  expect(outputSignup.message).toBe("Invalid CPF")
+})
